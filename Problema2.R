a33fba2ff7371cdbc0053337149cc077a2a0db20
shreeya.baniya <shreeya.baniya@auleseebe.upc.edu>
2025-02-11 09:50:35 +0100

    respuestas p2

--- Problema2.R
+++ Problema2.R
@@ -1,1 +1,32 @@
 #Codigo para problema 2
+
+#tabla de frecuencias
+mis_dades <- mtcars
+mis_dades$cyl
+
+#frecuencia absoluta
+ni<- table(mis_dades$cyl)
+barplot(ni)
+fi<- ni/length(mis_dades$cyl)
+pie(fi)
+Ni<- cumsum(ni)
+Ni
+#frecuencia relativa acumulada
+Fi<- Ni/length(mis_dades$cyl)
+Fi
+
+cbind(ni, fi, Ni, Fi)
+
+#tabla de frecuencias para 
+
+x<- mis_dades$mpg
+min(x)
+max(x)
+y<- cut(x,10)
+ni<- table(y)
+fi<- ni/length(y)
+Ni<- cumsum(ni)
+Fi<- Ni/length(y)
+cbind(ni,fi,Ni,Fi)
+
+hist(mis_dades$mpg)
