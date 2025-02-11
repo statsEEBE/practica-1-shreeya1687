9e3c85f68bd5087e8fff1b5e94d73de2d185f1f5
shreeya.baniya <shreeya.baniya@auleseebe.upc.edu>
2025-02-11 09:28:48 +0100

    respuestasss

--- Problema1.R
+++ Problema1.R
@@ -1,3 +1,25 @@
 #Codigo para problema 1
 
-plot(1:5)
+mis_dades <- mtcars
+mis_dades
+dim(mis_dades)
+
+mis_dades$qsec
+
+#promedio
+mean(mis_dades$qsec)
+
+sort(mis_dades$drat)
+median(mis_dades$drat)
+quantile(mis_dades$drat,0.75)
+IQR(mis_dades$drat)
+
+boxplot(mis_dades$drat)
+quantile(mis_dades$mpg, 0.18)
+IQR(mis_dades$cyl)
+sd(mis_dades$cyl)
+var(mis_dades$qsec)
+x<- mis_dades$qsec
+sum((x-mean(x))^2)/(length(x)-1)
+var(x)
+    
